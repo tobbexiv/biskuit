917a0192da9d2dada164f358554b940442a4e67d
Tobbe <contact@tobbexiv.de>
2020-05-02 14:36:33 +0200

    Update Finder/Storage

--- app/system/modules/finder/webpack.config.js
+++ app/system/modules/finder/webpack.config.js
@@ -3,11 +3,19 @@ module.exports = [
     {
         entry: {
             "panel-finder": "./app/components/panel-finder.vue",
-            "link-storage": "./app/components/link-storage.vue"
         },
         output: {
             filename: "./app/bundle/[name].js",
             library: "Finder"
+        }
+    },
+
+    {
+        entry: {
+            "link-storage": "./app/components/link-storage.vue"
+        },
+        output: {
+            filename: "./app/bundle/[name].js"
         }
     }
 
