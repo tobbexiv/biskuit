f6cecb92cf9c9365f6fc26854df96464105f9b79
Tobbe <contact@tobbexiv.de>
2020-06-07 18:24:05 +0200

    Update debugbar

--- app/modules/debug/app/components/auth.vue
+++ app/modules/debug/app/components/auth.vue
@@ -1,59 +1,46 @@
 <template>
-
-    <a title="User"><span class="pf-icon pf-icon-auth" :class="{'pf-parent': data.user}"></span>{{ label }}</a>
-
-    <div class="pf-dropdown" v-show="data.user">
-
-        <table class="pf-table pf-table-dropdown">
-            <tbody>
-                <tr>
-                    <td>Username</td>
-                    <td>{{ data.user }}</td>
-                </tr>
-                <tr>
-                    <td>Roles</td>
-                    <td>{{ data.roles | json }}</td>
-                </tr>
-                <tr>
-                    <td>Authenticated</td>
-                    <td>{{ data.authenticated ? 'yes' : 'no' }}</td>
-                </tr>
-                <tr>
-                    <td>Class</td>
-                    <td>{{ data.user_class }}</td>
-                </tr>
-            </tbody>
-        </table>
-
-    </div>
-
+    <li>
+        <a title="User"><span class="pf-icon pf-icon-auth" :class="{'pf-parent': data.user}"></span>{{ label }}</a>
+        <div class="pf-dropdown" v-if="data.user">
+            <table class="pf-table pf-table-dropdown">
+                <tbody>
+                    <tr>
+                        <td>Username</td>
+                        <td>{{ data.user }}</td>
+                    </tr>
+                    <tr>
+                        <td>Roles</td>
+                        <td>{{ data.roles }}</td>
+                    </tr>
+                    <tr>
+                        <td>Authenticated</td>
+                        <td>{{ data.authenticated ? 'yes' : 'no' }}</td>
+                    </tr>
+                    <tr>
+                        <td>Class</td>
+                        <td>{{ data.user_class }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </li>
 </template>
 
 <script>
-
-    module.exports = {
-
+    export default {
         section: {
             priority: 60
         },
 
         props: ['data'],
 
-        replace: false,
-
         computed: {
-
-            label: function () {
-
+            label() {
                 if (this.data.user) {
                     return this.data.user;
                 }
-
                 return this.data.enabled ? 'You are not authenticated.' : 'Authentication is disabled.';
             }
-
         }
-
     };
-
 </script>
