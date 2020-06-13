d7cedde3ece246f337c97858467e4d27aa75c650
Tobbe <contact@tobbexiv.de>
2020-06-13 17:34:10 +0200

    Update blog

--- packages/biskuit/blog/app/components/post-settings.vue
+++ packages/biskuit/blog/app/components/post-settings.vue
@@ -2,33 +2,33 @@
 
     <div class="uk-grid pk-grid-large pk-width-sidebar-large uk-form-stacked" data-uk-grid-margin>
         <div class="pk-width-content">
-
+            <v-validated-input
+                id="form-title"
+                name="title"
+                rules="required"
+                placeholder="Enter Title"
+                :error-messages="{ required: 'Title cannot be blank.' }"
+                :options="{ innerWrapperClass: '', elementClass: 'uk-width-1-1 uk-form-large' }"
+                v-model="post.title">
+            </v-validated-input>
             <div class="uk-form-row">
-                <input class="uk-width-1-1 uk-form-large" type="text" name="title" :placeholder="'Enter Title' | trans" v-model="post.title" v-validate:required>
-                <p class="uk-form-help-block uk-text-danger" v-show="form.title.invalid">{{ 'Title cannot be blank.' | trans }}</p>
-            </div>
-            <div class="uk-form-row">
-                <v-editor id="post-content" :value.sync="post.content" :options="{markdown : post.data.markdown}"></v-editor>
+                <v-editor id="post-content" v-model="post.content" :options="{ markdown : post.data.markdown }"></v-editor>
             </div>
             <div class="uk-form-row">
                 <label for="form-post-excerpt" class="uk-form-label">{{ 'Excerpt' | trans }}</label>
                 <div class="uk-form-controls">
-                    <v-editor id="post-excerpt" :value.sync="post.excerpt" :options="{markdown : post.data.markdown, height: 250}"></v-editor>
+                    <v-editor id="post-excerpt" v-model="post.excerpt" :options="{ markdown : post.data.markdown, height: 250 }"></v-editor>
                 </div>
             </div>
-
         </div>
         <div class="pk-width-sidebar">
-
             <div class="uk-panel">
-
                 <div class="uk-form-row">
                     <label for="form-image" class="uk-form-label">{{ 'Image' | trans }}</label>
                     <div class="uk-form-controls">
-                        <input-image-meta :image.sync="post.data.image" class="pk-image-max-height"></input-image-meta>
+                        <input-image-meta v-model="post.data.image" cls="pk-image-max-height"></input-image-meta>
                     </div>
                 </div>
-
                 <div class="uk-form-row">
                     <label for="form-slug" class="uk-form-label">{{ 'Slug' | trans }}</label>
                     <div class="uk-form-controls">
@@ -39,7 +39,7 @@
                     <label for="form-status" class="uk-form-label">{{ 'Status' | trans }}</label>
                     <div class="uk-form-controls">
                         <select id="form-status" class="uk-width-1-1" v-model="post.status">
-                            <option v-for="(id, status) in data.statuses" :value="id">{{status}}</option>
+                            <option v-for="(status, id) in data.statuses" :value="id" :key="id">{{status}}</option>
                         </select>
                     </div>
                 </div>
@@ -47,22 +47,21 @@
                     <label for="form-author" class="uk-form-label">{{ 'Author' | trans }}</label>
                     <div class="uk-form-controls">
                         <select id="form-author" class="uk-width-1-1" v-model="post.user_id">
-                            <option v-for="author in data.authors" :value="author.id">{{author.username}}</option>
+                            <option v-for="author in data.authors" :value="author.id" :key="author.id">{{author.username}}</option>
                         </select>
                     </div>
                 </div>
                 <div class="uk-form-row">
                     <span class="uk-form-label">{{ 'Publish on' | trans }}</span>
                     <div class="uk-form-controls">
-                        <input-date :datetime.sync="post.date"></input-date>
+                        <input-date v-model="post.date"></input-date>
                     </div>
                 </div>
-
                 <div class="uk-form-row">
                     <span class="uk-form-label">{{ 'Restrict Access' | trans }}</span>
                     <div class="uk-form-controls uk-form-controls-text">
-                        <p v-for="role in data.roles" class="uk-form-controls-condensed">
-                            <label><input type="checkbox" :value="role.id" v-model="post.roles" number> {{ role.name }}</label>
+                        <p v-for="role in data.roles" class="uk-form-controls-condensed" :key="role.id">
+                            <label><input type="checkbox" :value="role.id" v-model.number="post.roles"> {{ role.name }}</label>
                         </p>
                     </div>
                 </div>
@@ -75,24 +74,35 @@
                         <label><input type="checkbox" v-model="post.comment_status" value="1"> {{ 'Enable Comments' | trans }}</label>
                     </div>
                 </div>
-
             </div>
-
         </div>
     </div>
-
 </template>
 
 <script>
-
-    module.exports = {
-
-        props: ['post', 'data', 'form'],
+    export default {
+        props: ['data', 'value'],
 
         section: {
             label: 'Post'
+        },
+
+        data() {
+            return {
+                post: this.value
+            };
+        },
+
+        watch: {
+            value(newPost) {
+                this.post = newPost;
+            },
+            post: {
+                handler(newPost) {
+                    this.$emit('input', newPost);
+                },
+                deep: true
+            }
         }
-
     };
-
 </script>
--- packages/biskuit/blog/app/views/comments.js
+++ packages/biskuit/blog/app/views/comments.js
@@ -1,8 +1,7 @@
-module.exports = {
-
+const BlogComments = {
     el: '#comments',
 
-    data: function () {
+    data() {
         return _.extend({
             post: {},
             tree: {},
@@ -13,141 +12,148 @@ module.exports = {
         }, window.$comments);
     },
 
-    created: function () {
+    created() {
         this.load();
     },
 
     methods: {
+        load() {
+            return this.$http.get('api/blog/comment{/id}', { post: this.config.post }).then(function (res) {
+                var { data } = res;
 
-        load: function () {
+                this.comments = data.comments;
+                this.tree = this.getTree(data.comments);
+                this.post = data.posts[0];
+                this.count = data.count;
 
-            return this.$http.get('api/blog/comment{/id}', {post: this.config.post}).then(function (res) {
-                var data = res.data;
-
-                this.$set('comments', data.comments);
-                this.$set('tree', _.groupBy(data.comments, 'parent_id'));
-                this.$set('post', data.posts[0]);
-                this.$set('count', data.count);
-
-                this.$nextTick(function () {
-                    var anchor = jQuery(window.location.hash);
-
-                    if (anchor && anchor.length) {
-                        UIkit.Utils.scrollToElement(anchor);
+                Vue.nextTick(() => {
+                    if(window.location.hash) {
+                        const anchor = document.querySelector(window.location.hash);
+                        UIkit.scroll().scrollTo(anchor);
                     }
                 });
-
                 this.reply();
             });
         },
 
-        reply: function (parent) {
+        getTree(comments) {
+            const flattenRecursive = (groupedTree, parentNode) => {
+                let nodes = [ parentNode ];
+                if(groupedTree[parentNode.id]) {
+                    groupedTree[parentNode.id].forEach(node => {
+                        nodes = nodes.concat(flattenRecursive(groupedTree, node));
+                    });
+                }
+                return nodes;
+            };
+            const resolveTreeRecursive = (groupedTree, parentId, depth) => {
+                let tree = {};
+                if(groupedTree[parentId]) {
+                    if(depth < this.config.max_depth) {
+                        tree[parentId] = groupedTree[parentId];
+                        groupedTree[parentId].forEach(node => {
+                            tree = _.merge(tree, resolveTreeRecursive(groupedTree, node.id, depth + 1));
+                        });
+                    } else {
+                        tree[parentId] = _.tail(flattenRecursive(groupedTree, { id: parentId }));
+                    }
+                }
+                return tree;
+            };
+            return resolveTreeRecursive(_.groupBy(comments, 'parent_id'), '0', 1);
+        },
 
+        reply(parent) {
             parent = parent || this;
-
             if (this.replyForm) {
+                this.replyForm.$parent.$refs.reply.removeChild(this.replyForm.$el);
                 this.replyForm.$destroy(true);
             }
 
-            this.replyForm = new this.$options.components['reply']({
-                data: {config: this.config, parent: parent.comment && parent.comment.id || 0},
+            const Reply = Vue.extend(this.$options.components['reply'])
+            this.replyForm = new Reply({
+                data: {
+                    config: this.config,
+                    parent: parent.comment && parent.comment.id || 0
+                },
                 parent: parent
-            }).$mount().$appendTo(parent.$els.reply);
-
+            }).$mount();
+            parent.$refs.reply.appendChild(this.replyForm.$el);
         }
-
     },
 
     components: {
-
         comment: {
-
             name: 'Comment',
-            props: ['comment'],
+            props: ['comment', 'tree'],
             template: '#comments-item',
 
-            data: function () {
+            data() {
                 return {
-                    config: this.$root.config,
-                    tree: this.$root.tree
+                    config: this.$root.config
                 };
             },
 
             computed: {
+                depth() {
+                    let depth = 1;
+                    let parent = this.$parent;
 
-                depth: function () {
-
-                    var depth = 1, parent = this.$parent;
-
-                    while (parent) {
+                    while(parent) {
                         if (parent.$options.name === 'Comment') {
                             depth++;
                         }
                         parent = parent.$parent;
                     }
-
                     return depth;
                 },
 
-                showReplyButton: function () {
-                    return this.config.enabled && !this.isLeaf && this.$root.replyForm.$parent !== this;
+                showReplyButton() {
+                    return this.config.enabled && this.depth < this.config.max_depth && this.$root.replyForm.$parent !== this;
                 },
 
-                remainder: function () {
-                    return this.isLeaf && this.tree[this.comment.id] || [];
-                },
-
-                isLeaf: function () {
-                    return this.depth >= this.config.max_depth;
-                },
-
-                permalink: function () {
+                permalink() {
                     return '#comment-' + this.comment.id;
                 }
 
             },
 
             methods: {
-
-                replyTo: function () {
+                replyTo() {
                     this.$root.reply(this);
-                }
+                },
 
+                setHash() {
+                    window.location.hash = this.permalink;
+                },
             }
-
         },
 
         reply: {
-
             template: '#comments-reply',
 
-            data: function () {
+            data() {
                 return {
                     author: '',
                     email: '',
                     content: '',
                     error: false,
-                    form: false
                 };
             },
 
             computed: {
-
-                user: function () {
+                user() {
                     return this.config.user;
                 },
 
-                login: function () {
-                    return this.$url('user/login', {redirect: window.location.href});
+                login() {
+                    return this.$url('user/login', { redirect: window.location.href });
                 }
-
             },
 
             methods: {
-
-                save: function () {
-
-                    var comment = {
+                save() {
+                    let comment = {
                         parent_id: this.parent,
                         post_id: this.config.post,
                         content: this.content
@@ -158,42 +164,29 @@ module.exports = {
                         comment.email = this.email;
                     }
 
-                    this.$set('error', false);
-
-                    this.$resource('api/blog/comment{/id}').save({id: 0}, {comment: comment}).then(function (res) {
-
-                        var data = res.data;
-
+                    this.error = false;
+                    this.$resource('api/blog/comment{/id}').save({ id: 0 }, { comment: comment }).then(function (res) {
+                        const { data } = res;
                         if (!this.user.skipApproval) {
-
                             this.$root.messages.push(this.$trans('Thank you! Your comment needs approval before showing up.'));
-
                         } else {
-
-                            this.$root.load().then(function () {
+                            this.$root.load().then(() => {
                                 window.location.hash = 'comment-' + data.comment.id;
                             });
                         }
-
                         this.cancel()
-
                     }, function () {
-
                         // TODO better error messages
-                        this.$set('error', this.$trans('Unable to comment. Please try again later.'));
+                        this.error = this.$trans('Unable to comment. Please try again later.');
                     });
                 },
 
-                cancel: function () {
+                cancel() {
                     this.$root.reply();
                 }
-
             }
-
         }
-
     }
-
 };
 
-Vue.ready(module.exports);
+Vue.ready(BlogComments);
