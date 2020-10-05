d340ec3ec1e6c488333cf75ff71d70f26dca5d23
mirkobrombin <send@mirko.pm>
2020-10-05 17:44:47 +0200

    Move site module to Uikit 3 This task is not complete

--- app/system/modules/site/views/admin/index.php
+++ app/system/modules/site/views/admin/index.php
@@ -1,108 +1,148 @@
 <?php $view->script('site-index', 'system/site:app/bundle/index.js', ['vue', 'uikit-nestable']) ?>
 
 <div id="site" class="uk-form" v-cloak>
-    <div class="uk-grid pk-grid-large" data-uk-grid-margin>
-        <div class="pk-width-sidebar">
-            <div class="uk-panel">
-                <ul class="uk-nav uk-nav-side">
-                    <li class="uk-visible-toggle" :class="{'uk-active': isActive(menu), 'uk-nav-divider': menu.divider}" v-for="menu in menusWithDivider">
-                        <a @click="selectMenu(menu, false)" v-if="!menu.divider">{{ menu.label }}</a>
-                        <ul class="uk-subnav pk-subnav-icon uk-hidden" v-if="!menu.fixed && !menu.divider">
-                            <li><a class="pk-icon-edit pk-icon-hover" :title="$trans('Edit')" data-uk-tooltip="{delay: 500}" @click="editMenu(menu)"></a></li>
-                            <li><a class="pk-icon-delete pk-icon-hover" :title="$trans('Delete')" data-uk-tooltip="{delay: 500}" @click="removeMenu(menu)" v-confirm="'Delete menu?'"></a></li>
-                        </ul>
-                    </li>
-                </ul>
+  <div uk-grid>
 
-                <p>
-                    <a class="uk-button" @click.prevent="editMenu()">{{ 'Add Menu' | trans }}</a>
-                </p>
-            </div>
-        </div>
-        <div class="pk-width-content">
-            <div class="uk-margin uk-flex uk-flex-space-between uk-flex-wrap" data-uk-margin>
-                <div class="uk-flex uk-flex-middle uk-flex-wrap" data-uk-margin>
-                    <h2 class="uk-margin-remove">{{ menu.label | trans }}</h2>
-                    <div class="uk-margin-left" v-show="selected.length">
-                        <ul class="uk-subnav pk-subnav-icon">
-                            <li><a class="pk-icon-check pk-icon-hover" :title="$trans('Publish')" data-uk-tooltip="{delay: 500}" @click="status(1)"></a></li>
-                            <li><a class="pk-icon-block pk-icon-hover" :title="$trans('Unpublish')" data-uk-tooltip="{delay: 500}" @click="status(0)"></a></li>
-                            <li v-show="showMove" data-uk-dropdown="{ mode: 'click' }">
-                                <a class="pk-icon-move pk-icon-hover" :title="$trans('Move')" data-uk-tooltip="{delay: 500}" @click.prevent></a>
-                                <div class="uk-dropdown uk-dropdown-small">
-                                    <ul class="uk-nav uk-nav-dropdown">
-                                        <li v-for="m in menusWithoutTrash"><a @click="moveNodes(m.id)">{{ m.label }}</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li v-show="showDelete"><a class="pk-icon-delete pk-icon-hover" :title="$trans('Delete')" data-uk-tooltip="{delay: 500}" @click="removeNodes" v-confirm="'Delete item?'"></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="uk-position-relative" data-uk-margin>
-                    <div data-uk-dropdown="{ mode: 'click' }">
-                        <a class="uk-button uk-button-primary" @click.prevent v-show="menu.id != 'trash'">{{ 'Add Page' | trans }}</a>
-                        <div class="uk-dropdown uk-dropdown-small uk-dropdown-flip">
-                            <ul class="uk-nav uk-nav-dropdown">
-                                <li v-for="type in unprotectedTypes">
-                                    <a :href="$url.route('admin/site/page/edit', { id: type.id, menu: menu.id })">{{ type.label }}</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="uk-overflow-container">
-                <div class="pk-table-fake pk-table-fake-header" :class="{'pk-table-fake-border': !tree[0]}">
-                    <div class="bk-table-width-minimum pk-table-fake-nestable-padding"><input type="checkbox" v-check-all:nodes.number="{ watchedElementsSelector: 'input[name=id]', statusStorageSelector: 'selected' }"></div>
-                    <div class="pk-table-min-width-100">{{ 'Title' | trans }}</div>
-                    <div class="bk-table-width-100 uk-text-center">{{ 'Status' | trans }}</div>
-                    <div class="bk-table-width-100">{{ 'Type' | trans }}</div>
-                    <div class="bk-table-width-150">{{ 'URL' | trans }}</div>
-                </div>
-
-                <ul class="uk-nestable uk-margin-remove" ref="nestable" v-show="tree[0]">
-                    <node v-for="node in tree[0]" :tree="tree" :node="node" :key="`${node.id}`" v-model="selected"></node>
-                </ul>
-            </div>
-            <h3 class="uk-h1 uk-text-muted uk-text-center" v-show="tree && !tree[0]">{{ 'No pages found.' | trans }}</h3>
-        </div>
+    <div class="uk-width-auto@m">
+      <ul class="uk-tab-left" uk-tab="connect: #component-tab-left; animation: uk-animation-fade" data-uk-tab="{ connect: '#tab-content' }">
+        <li class="uk-visible-toggle" :class="{'uk-active': isActive(menu), 'uk-nav-divider': menu.divider}" v-for="menu in menusWithDivider">
+          <a @click="selectMenu(menu, false)" v-if="!menu.divider">{{ menu.label }}</a>
+          <ul class="uk-subnav pk-subnav-icon uk-hidden" v-if="!menu.fixed && !menu.divider">
+            <li><a class="pk-icon-edit pk-icon-hover" :title="$trans('Edit')" data-uk-tooltip="{delay: 500}" @click="editMenu(menu)"></a></li>
+            <li><a class="pk-icon-delete pk-icon-hover" :title="$trans('Delete')" data-uk-tooltip="{delay: 500}" @click="removeMenu(menu)" v-confirm="'Delete menu?'"></a></li>
+          </ul>
+        </li>
+        <li>
+          <button class="uk-button uk-button-primary" @click.prevent="editMenu()">{{ 'Add Menu' | trans }}</button>
+        </li>
+      </ul>
     </div>
 
-    <v-modal ref="modal">
-        <validation-observer v-slot="{ handleSubmit }" slim>
-            <form class="uk-form uk-form-stacked" @submit.prevent="handleSubmit(function() { saveMenu(edit); })">
-                <div class="uk-modal-header">
-                    <h2>{{ 'Add Menu' | trans }}</h2>
-                </div>
+    <div class="uk-width-expand@m">
+      <ul id="component-tab-left" class="uk-switcher">
 
-                <v-validated-input
-                    id="form-name"
-                    name="label"
-                    rules="required"
-                    label="Name"
-                    :error-messages="{ required: 'Name cannot be blank.' }"
-                    :options="{ elementClass: 'uk-width-1-1 uk-form-large' }"
-                    v-model="edit.label">
-                </v-validated-input>
-
-                <div class="uk-margin">
-                    <span class="uk-form-label">{{ 'Menu Positions' | trans }}</span>
-                    <div class="uk-form-controls uk-form-controls-text">
-                        <p class="uk-form-controls-condensed" v-for="m in config.menus">
-                            <label><input type="checkbox" :value="m.name" v-model="edit.positions"> {{ m.label }}</label> <span class="uk-text-muted" v-if="getMenu(m.name) && getMenu(m.name).id != edit.id">{{ '(Currently set to: %menu%)' | trans({ menu: getMenu(m.name).label }) }}</span>
-                        </p>
+        <li>
+          <div class="uk-margin uk-flex uk-flex-between uk-flex-wrap" data-uk-margin>
+            <div data-uk-margin>
+              <h2 class="uk-margin-remove">{{ menu.label | trans }}</h2>
+              <div class="uk-margin-left" v-show="selected.length">
+                <ul class="uk-iconnav">
+                  <li><a :title="$trans('Publish')" data-uk-tooltip="{delay: 500}" @click="status(1)"><span uk-icon="check"></span></a></li>
+                  <li><a :title="$trans('Unpublish')" data-uk-tooltip="{delay: 500}" @click="status(0)"><span uk-icon="close"></span></a></li>
+                  <li v-show="showMove" data-uk-dropdown="{ mode: 'click' }">
+                    <a :title="$trans('Move')" data-uk-tooltip="{delay: 500}" @click.prevent></a>
+                    <div class="uk-dropdown uk-dropdown-small">
+                      <ul class="uk-nav uk-nav-dropdown">
+                        <li v-for="m in menusWithoutTrash"><a @click="moveNodes(m.id)">{{ m.label }}</a></li>
+                      </ul>
                     </div>
-                </div>
+                  </li>
+                  <li v-show="showDelete"><a :title="$trans('Delete')" data-uk-tooltip="{delay: 500}" @click="removeNodes" v-confirm="'Delete item?'"><span uk-icon="trash"></span></a></li>
+                </ul>
+              </div>
+            </div>
+            <div data-uk-margin>
+              <a class="uk-button uk-button-primary" @click.prevent v-show="menu.id != 'trash'">{{ 'Add Page' | trans }}</a>
+              <div uk-dropdown="mode: click">
+                <ul>
+                  <li v-for="type in unprotectedTypes">
+                    <a :href="$url.route('admin/site/page/edit', { id: type.id, menu: menu.id })">{{ type.label }}</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="uk-margin">
 
-                <div class="uk-modal-footer uk-text-right">
-                    <button class="uk-button uk-button-link uk-modal-close" type="button" @click.prevent="cancel">{{ 'Cancel' | trans }}</button>
-                    <button class="uk-button uk-button-link" :disabled="!edit.label">{{ 'Save' | trans }}</button>
-                </div>
-            </form>
+            <div class="uk-overflow-container">
+              <div class="pk-table-fake pk-table-fake-header" :class="{'pk-table-fake-border': !tree[0]}">
+                <div class="bk-table-width-minimum pk-table-fake-nestable-padding"><input type="checkbox" v-check-all:nodes.number="{ watchedElementsSelector: 'input[name=id]', statusStorageSelector: 'selected' }"></div>
+                <div class="pk-table-min-width-100">{{ 'Title' | trans }}</div>
+                <div class="bk-table-width-100 uk-text-center">{{ 'Status' | trans }}</div>
+                <div class="bk-table-width-100">{{ 'Type' | trans }}</div>
+                <div class="bk-table-width-150">{{ 'URL' | trans }}</div>
+              </div>
+              <ul class="uk-nestable uk-margin-remove" ref="nestable" v-show="tree[0]">
+                <node v-for="node in tree[0]" :tree="tree" :node="node" :key="`${node.id}`" v-model="selected"></node>
+              </ul>
+            </div>
+            <h3 class="uk-h1 uk-text-muted uk-text-center" v-show="tree && !tree[0]">{{ 'No pages found.' | trans }}</h3>
+
+          </div>
+        </li>
+
+      </ul>
+
+      <v-modal ref="modal">
+        <validation-observer v-slot="{ handleSubmit }" slim>
+          <form class="uk-form uk-form-stacked" @submit.prevent="handleSubmit(function() { saveMenu(edit); })">
+            <div class="uk-modal-header">
+              <h2>{{ 'Add Menu' | trans }}</h2>
+            </div>
+
+            <v-validated-input
+                id="form-name"
+                name="label"
+                rules="required"
+                label="Name"
+                :error-messages="{ required: 'Name cannot be blank.' }"
+                :options="{ elementClass: 'uk-width-1-1 uk-form-large' }"
+                v-model="edit.label">
+            </v-validated-input>
+
+            <div class="uk-margin">
+              <span class="uk-form-label">{{ 'Menu Positions' | trans }}</span>
+              <div class="uk-form-controls uk-form-controls-text">
+                <p class="uk-form-controls-condensed" v-for="m in config.menus">
+                  <label><input type="checkbox" :value="m.name" v-model="edit.positions"> {{ m.label }}</label> <span class="uk-text-muted" v-if="getMenu(m.name) && getMenu(m.name).id != edit.id">{{ '(Currently set to: %menu%)' | trans({ menu: getMenu(m.name).label }) }}</span>
+                </p>
+              </div>
+            </div>
+
+            <div class="uk-modal-footer uk-text-right">
+              <button class="uk-button uk-button-link uk-modal-close" type="button" @click.prevent="cancel">{{ 'Cancel' | trans }}</button>
+              <button class="uk-button uk-button-link" :disabled="!edit.label">{{ 'Save' | trans }}</button>
+            </div>
+          </form>
         </validation-observer>
-    </v-modal>
+      </v-modal>
+    </div>
+
+  </div>
+  <v-modal ref="modal">
+    <validation-observer v-slot="{ handleSubmit }" slim>
+      <form class="uk-form uk-form-stacked" @submit.prevent="handleSubmit(function() { saveMenu(edit); })">
+        <div class="uk-modal-header">
+          <h2>{{ 'Add Menu' | trans }}</h2>
+        </div>
+
+        <v-validated-input
+            id="form-name"
+            name="label"
+            rules="required"
+            label="Name"
+            :error-messages="{ required: 'Name cannot be blank.' }"
+            :options="{ elementClass: 'uk-width-1-1 uk-form-large' }"
+            v-model="edit.label">
+        </v-validated-input>
+
+        <div class="uk-margin">
+          <span class="uk-form-label">{{ 'Menu Positions' | trans }}</span>
+          <div class="uk-form-controls uk-form-controls-text">
+            <p class="uk-form-controls-condensed" v-for="m in config.menus">
+              <label><input type="checkbox" :value="m.name" v-model="edit.positions"> {{ m.label }}</label> <span class="uk-text-muted" v-if="getMenu(m.name) && getMenu(m.name).id != edit.id">{{ '(Currently set to: %menu%)' | trans({ menu: getMenu(m.name).label }) }}</span>
+            </p>
+          </div>
+        </div>
+
+        <div class="uk-modal-footer uk-text-right">
+          <button class="uk-button uk-button-link uk-modal-close" type="button" @click.prevent="cancel">{{ 'Cancel' | trans }}</button>
+          <button class="uk-button uk-button-link" :disabled="!edit.label">{{ 'Save' | trans }}</button>
+        </div>
+      </form>
+    </validation-observer>
+  </v-modal>
+
 </div>
 
 <script id="node" type="text/template">
