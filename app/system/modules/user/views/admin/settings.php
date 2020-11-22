871b4a5bc5efd51ae69a009c2e91605d43b774b7
Tobbe <contact@tobbexiv.de>
2020-11-22 15:31:44 +0100

    Update user settings.

--- app/system/modules/user/views/admin/settings.php
+++ app/system/modules/user/views/admin/settings.php
@@ -1,12 +1,12 @@
 <?php $view->script('settings', 'system/user:app/bundle/settings.js', ['vue', 'input-link']) ?>
 
-<div id="settings" class="uk-form uk-form-horizontal" v-cloak>
+<div id="settings" class="uk-form-horizontal" v-cloak>
 
-    <div class="uk-margin uk-flex uk-flex-space-between uk-flex-wrap" data-uk-margin>
-        <div data-uk-margin>
+    <div class="uk-margin uk-flex uk-flex-between uk-flex-wrap" uk-margin>
+        <div uk-margin>
             <h2 class="uk-margin-remove">{{ 'Settings' | trans }}</h2>
         </div>
-        <div data-uk-margin>
+        <div uk-margin>
             <button class="uk-button uk-button-primary" @click.prevent="save">{{ 'Save' | trans }}</button>
         </div>
     </div>
@@ -14,14 +14,14 @@
     <div class="uk-margin">
         <span class="uk-form-label">{{ 'Registration' | trans }}</span>
         <div class="uk-form-controls uk-form-controls-text">
-            <p class="uk-form-controls-condensed">
-                <label><input type="radio" v-model="config.registration" value="admin"> {{ 'Disabled' | trans }}</label>
+            <p class="uk-margin-small">
+                <label><input class="uk-radio" type="radio" v-model="config.registration" value="admin"> {{ 'Disabled' | trans }}</label>
             </p>
-            <p class="uk-form-controls-condensed">
-                <label><input type="radio" v-model="config.registration" value="guest"> {{ 'Enabled' | trans }}</label>
+            <p class="uk-margin-small">
+                <label><input class="uk-radio" type="radio" v-model="config.registration" value="guest"> {{ 'Enabled' | trans }}</label>
             </p>
-            <p class="uk-form-controls-condensed">
-                <label><input type="radio" v-model="config.registration" value="approval"> {{ 'Enabled, but approval is required.' | trans }}</label>
+            <p class="uk-margin-small">
+                <label><input class="uk-radio" type="radio" v-model="config.registration" value="approval"> {{ 'Enabled, but approval is required.' | trans }}</label>
             </p>
         </div>
     </div>
@@ -29,7 +29,7 @@
     <div class="uk-margin">
         <label for="form-user-verification" class="uk-form-label">{{ 'Verification' | trans }}</label>
         <div class="uk-form-controls uk-form-controls-text">
-            <label><input id="form-user-verification" type="checkbox" v-model="config.require_verification"> {{ 'Require e-mail verification when a guest creates an account.' | trans }}</label>
+            <label><input id="form-user-verification" class="uk-checkbox" type="checkbox" v-model="config.require_verification"> {{ 'Require e-mail verification when a guest creates an account.' | trans }}</label>
         </div>
     </div>
 
